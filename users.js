19f162aa36a40d06f42a7cade9a0729e66e2d604
Emi Tsukuda <gioramlevi13@gmail.com>
2019-09-25 01:03:12 -0700

    sucessfully response with json data

--- users.js
+++ users.js
@@ -3,7 +3,7 @@ const csv = require('csv-parser');
 const fs = require('fs');
 
 const express = require("express");
-const results = []
+const results;
 const router = new express.Router();
 
 // GET--- endpoint for getting all appointment
@@ -11,11 +11,11 @@ router.get('/', async function (req, res, next) {
     try {
         fs.createReadStream('users.csv')
         .pipe(csv())
-        .on('data', (data) => { console.log(data) })
+        .on('data', (data) => results = data)
         .on('end', () => {console.log('CSV file successfully processed');
         });
        
-        return res.json({results});
+        return res.json( {results} );
     } catch (err) {
         return next(err)
     }
