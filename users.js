5c6486e367283d74997cd35535e580914e6575d1
Emi Tsukuda <gioramlevi13@gmail.com>
2019-09-25 00:56:45 -0700

    successfully read from cvs file

--- users.js
+++ users.js
@@ -1,13 +1,21 @@
 // Endpoints for user 
+const csv = require('csv-parser');
+const fs = require('fs');
 
 const express = require("express");
+const results = []
 const router = new express.Router();
 
 // GET--- endpoint for getting all appointment
 router.get('/', async function (req, res, next) {
     try {
+        fs.createReadStream('users.csv')
+        .pipe(csv())
+        .on('data', (data) => { console.log(data) })
+        .on('end', () => {console.log('CSV file successfully processed');
+        });
        
-        return res.send("hello");
+        return res.json({results});
     } catch (err) {
         return next(err)
     }
