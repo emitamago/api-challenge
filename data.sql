319ee741a03bd20245606bbfce1ba692b918ceee
Emi Tsukuda <gioramlevi13@gmail.com>
2019-09-26 00:50:44 -0700

    sucessfully create db from csv file

---
+++ data.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS "api-challenge-one";
+CREATE DATABASE "api-challenge-one";
+\c "api-challenge-one"
+
+-- CREATE TABLE users (
+--     id serial PRIMARY KEY,
+--     username TEXT,
+--     userage INTEGER,
+--     usergender CHAR,
+--     lastlocation CHAR,
+--     lat INTEGER,
+--     long INTEGER,
+-- );
+CREATE TABLE users (
+  user_id INTEGER,
+  user_name TEXT NOT NULL,
+  user_age INTEGER,
+  user_gender CHAR,
+  last_location TEXT,
+  lat FLOAT,
+  long FLOAT
+);
+
+COPY users 
+FROM '/Users/emitsukuda/desktop/api-challenge-1/users.csv' 
+DELIMITER ',' CSV HEADER;
